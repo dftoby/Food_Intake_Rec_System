cc62fd6d97ebb3ef655a124ba68b6ea6f9fcb391
Yutong Lei <leiyutong8972@gmail.com>
2023-02-10 20:13:56 -0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Food_Rec_Intake_System',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
